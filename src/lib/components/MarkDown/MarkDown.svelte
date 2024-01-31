2859fd7b3da2fac08fb1c8f2cff04e1b4940ca08
beaudelaire <Beaudelaire.dev@gmail.com>
2024-01-30 19:26:04 -0500

    bug fix: dark mode disable for md

--- src/lib/components/MarkDown/MarkDown.svelte
+++ src/lib/components/MarkDown/MarkDown.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="mx-auto max-w-2xl space-y-12 px-4 pt-12 lg:px-0">
-    <article class="prose prose-lg prose-gray dark:prose-invert max-w-none prose-code:flex prose-code:flex-col transition-opacity duration-[1000ms] ease-in-out will-change-auto prose-code:[direction:ltr] prose-img:rounded-md prose-img:shadow-md prose-img:mx-auto prose-img:max-h-[clamp(400px,50vh,500px)] opacity-80">
+    <article class="prose prose-lg prose-invert max-w-none prose-code:flex prose-code:flex-col transition-opacity duration-[1000ms] ease-in-out will-change-auto prose-code:[direction:ltr] prose-img:rounded-md prose-img:shadow-md prose-img:mx-auto prose-img:max-h-[clamp(400px,50vh,500px)] opacity-80">
         {@html marked(source)}
     </article>
 </div>
